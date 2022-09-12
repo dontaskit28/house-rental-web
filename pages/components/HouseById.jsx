917142706d83daecf745b1aafb2bd96cf37ec47d
Tata Rao <103190168+rocky2801@users.noreply.github.com>
2022-09-12 23:38:41 +0530

    Update HouseById.jsx

--- pages/components/HouseById.jsx
+++ pages/components/HouseById.jsx
@@ -99,12 +99,7 @@ const HouseById = ({ house, houseid }) => {
               </h1>
               <div className="flex mb-4"></div>
               <div className="leading-relaxed">
-                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-                juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-                seitan poutine tumeric. Gastropub blue bottle austin listicle
-                pour-over, neutra jean shorts keytar banjo tattooed umami
-                cardigan.
+                {house.description}
               </div>
               <div className="flex justify-around mt-5">
                 <span className="title-font font-medium text-2xl text-gray-900">
